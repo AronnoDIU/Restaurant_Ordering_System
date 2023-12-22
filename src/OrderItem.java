3c81d91222ecc13d51fc4d3bade5f854769616f1
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-23 04:36:41 +0600

    Restaurant Ordering System

--- src/OrderItem.java
+++ src/OrderItem.java
@@ -4,3 +4,27 @@ record OrderItem(MenuItem menuItem, int quantity) {
         return menuItem.price() * quantity;
     }
 }
+
+// Or,
+
+/*class OrderItem {
+    private final MenuItem menuItem;
+    private final int quantity;
+
+    public OrderItem(MenuItem menuItem, int quantity) {
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double calculateItemTotal() {
+        return menuItem.getPrice() * quantity;
+    }
+}*/
