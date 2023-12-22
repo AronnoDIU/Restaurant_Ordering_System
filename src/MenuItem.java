cd1c2ad35d32dee712f80bca12800d6806c82359
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-23 01:20:58 +0600

    Restaurant Ordering System

---
+++ src/MenuItem.java
@@ -0,0 +1,17 @@
+class MenuItem {
+    private final String name;
+    private final double price;
+
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
